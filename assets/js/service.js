0efbaed2d5b292a3cc36faa633cc4a158229c45b
Walter Gandarella <walter.wgbn@gmail.com>
2016-04-30 15:21:31 -0300

    ordenação dos eventos ignorando eventos já ocorridos

--- assets/js/service.js
+++ assets/js/service.js
@@ -1,7 +1,9 @@
 agenda
     .factory('fireService', ["$firebaseArray",
         function($firebaseArray) {
-            var db = new Firebase("https://agenda-ti-ba.firebaseio.com/agenda").orderByChild('event_start');
+            var db = new Firebase("https://agenda-ti-ba.firebaseio.com/agenda").orderByChild('event_start').startAt(new Date().getTime()).on('event_end', function(snapshot) {
+                console.log('new record', snap.key());
+            });
             var sync = $firebaseArray(db);
 
             return {
